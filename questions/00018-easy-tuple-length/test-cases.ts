e82b7cc9e67d347a1fa1e63db008172da3e2415a
robyy <roby.yan84@gmail.com>
2023-05-16 17:49:26 -0400

    Length of (readonly) tuple

--- questions/00018-easy-tuple-length/test-cases.ts
+++ questions/00018-easy-tuple-length/test-cases.ts
@@ -1,13 +1,20 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type {Equal, Expect, NotEqual} from '@type-challenges/utils'
 
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
 const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+const Fruits = ['cherry', 'banana'] as const
+const Fruits2 = ['cherry2', 'banana2']
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
   Expect<Equal<Length<typeof spaceX>, 5>>,
-  // @ts-expect-error
+  Expect<Equal<Length<[1, 2, 3]>, 3>>,
+  Expect<NotEqual<Length<[1, 2, 3]>, 2>>,
+  Expect<Equal<Length<typeof Fruits>, 2>>,
+  Expect<Equal<Length<[]>, 0>>,
   Length<5>,
-  // @ts-expect-error
   Length<'hello world'>,
 ]
+
+console.log(typeof tesla) // readonly ['tesla', 'model 3', 'model X', 'model Y']
+console.log(typeof Fruits2) // string[], it's just array of string, it can have 0 OR more elements, too wide.
