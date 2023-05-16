51f08bc1557fe450b9a5523c69887e3d32825d03
robyy <roby.yan84@gmail.com>
2023-05-16 18:02:39 -0400

    readonly array is wider than regular/writable array.

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,2 +1,5 @@
-// we accept arrays, even readonly arrays (more specific, a special type of array)
+// writable/regular arrays is a subset of readonly array !
+// so readonly any[] is wider, more general, a more basic class than any[] (writable/regular array)
+// so all regular arrays can be treated as readonly arrays, BUT
+// readonly arrays CAN'T always be treated as writable arrays.
 type Length<T> = T extends readonly any[] ? T['length'] : never
