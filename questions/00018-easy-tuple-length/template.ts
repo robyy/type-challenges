61e74ef9bf3ea1ab8fff9205f30b766f9fc5e60d
robyy <roby.yan84@gmail.com>
2023-05-16 17:51:00 -0400

    Length of (readonly) tuple comment

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,2 +1,2 @@
-// we accept arrays, even readonly arrays (more specific)
+// we accept arrays, even readonly arrays (more specific, a special type of array)
 type Length<T> = T extends readonly any[] ? T['length'] : never
