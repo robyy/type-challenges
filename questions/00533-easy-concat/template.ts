e9b3ebb79e996df3336897440d4818625c6f8a19
robyy <roby.yan84@gmail.com>
2023-05-17 10:33:41 -0400

    concat tuples / arrays

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,3 @@
-type Concat<T, U> = any
+// Generic types like variable, when actually use the type, they will be "assigned" with
+// actual values, so can use the operator like destructure: `...`
+type Concat<A extends any[], B extends any[]> = [...A, ...B]
--- questions/00533-easy-concat/test-cases.ts
+++ questions/00533-easy-concat/test-cases.ts
@@ -5,4 +5,15 @@ type cases = [
   Expect<Equal<Concat<[], [1]>, [1]>>,
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
   Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], ['hello']>, ['hello']>>,
+  Expect<
+      Equal<Concat<[18, 19], [20, 21]>, [18, 19, 20, 21]>
+  >,
+  Expect<
+      Equal<
+          Concat<[42, 'a', 'b'], [Promise<boolean>]>,
+          [42, 'a', 'b', Promise<boolean>]
+      >
+  >,
 ]
